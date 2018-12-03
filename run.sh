4a1d104902350070dfaba6709f01d4f2aad7bd86
bob <iou2much@sina.com>
2018-12-03 13:31:59 +0800

    update

--- run.sh
+++ run.sh
@@ -11,9 +11,9 @@ if [ -n "$NODE_MEM" ]; then
     NODEJS_V8_ARGS="--max_old_space_size=$mem_node_old_space $NODEJS_V8_ARGS"
 fi
 
-if [ ! -d mywiki ]; then
-  /usr/bin/env node $NODEJS_V8_ARGS $tiddlywiki_script mywiki --init server
+if [ ! -d wiki ]; then
+  /usr/bin/env node $NODEJS_V8_ARGS $tiddlywiki_script wiki --init server
 fi
 
-exec /usr/bin/env node $NODEJS_V8_ARGS $tiddlywiki_script mywiki --server 8080 $:/core/save/all text/plain text/html ${USERNAME:-user} ${PASSWORD:-'wiki'} 0.0.0.0
+exec /usr/bin/env node $NODEJS_V8_ARGS $tiddlywiki_script wiki --server 8080 $:/core/save/all text/plain text/html ${USERNAME:-user} ${PASSWORD:-'wiki'} 0.0.0.0
 
